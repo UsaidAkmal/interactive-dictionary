faed8b707dbabb7831409a1f5bc249e279b95118
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-06-27 04:02:30 +0700

    start begining

---
+++ actdictionary.py
@@ -0,0 +1,12 @@
+PC = {
+    "mobo" : input("Apa mobo kamu : ")
+    "PSU" : input("Apa PSU kamu : ")
+    "VGA" : input("Apa VGA kamu : ")
+    "RAM" : input("Apa RAM kamu : ")
+    "storaged" : input("Apa Storaged kamu : ")
+    "software" : {
+        "OS" : input("Apa system operasi yang kamu pilih : ")
+        "python" : input("Versi python berapa yang kamu pilih : ")
+        "codeEditor" : input("Apa kode Editor yang kamu pilih : ")
+    }
+}
