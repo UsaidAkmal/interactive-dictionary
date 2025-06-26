b55ab64c307185e9da4a601033ff43001e629ad4
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-06-27 04:24:31 +0700

    start beggin

--- actdictionary.py
+++ actdictionary.py
@@ -1,12 +1,25 @@
+user = input("Maukan nama user : ")
+
 PC = {
-    "mobo" : input("Apa mobo kamu : ")
-    "PSU" : input("Apa PSU kamu : ")
-    "VGA" : input("Apa VGA kamu : ")
-    "RAM" : input("Apa RAM kamu : ")
-    "storaged" : input("Apa Storaged kamu : ")
+    "mobo" : input("Apa mobo kamu : "),
+    "PSU" : input("Apa PSU kamu : "),
+    "VGA" : input("Apa VGA kamu : "),
+    "RAM" : input("Apa RAM kamu : "),
+    "storaged" : input("Apa Storaged kamu : "),
     "software" : {
-        "OS" : input("Apa system operasi yang kamu pilih : ")
-        "python" : input("Versi python berapa yang kamu pilih : ")
+        "OS" : input("Apa system operasi yang kamu pilih : "),
+        "python" : input("Versi python berapa yang kamu pilih : "),
         "codeEditor" : input("Apa kode Editor yang kamu pilih : ")
     }
 }
+
+print(f'''
+Berikut adalah spesifikasi PC yang kamu sudah rakit
+
+    [+] PC {user} spesifikasinya adalah sebagai berikut:
+    [+] mobo : {PC["mobo"]}
+    [+] PSU : {PC["PSU"]}
+    [+] VGA : {PC["VGA"]}
+
+
+''')
